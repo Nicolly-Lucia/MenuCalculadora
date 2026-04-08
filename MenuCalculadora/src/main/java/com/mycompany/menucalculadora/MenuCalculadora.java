6fb439e07a70da484c566497d34820af1b609e1d
Nicolly Lucia Da Silva <nicklucia0401@gmail.com>
2026-04-08 13:30:37 -0300

    Add files via upload

---
+++ MenuCalculadora/src/main/java/com/mycompany/menucalculadora/MenuCalculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.menucalculadora;
+
+/**
+ *
+ * @author Admin
+ */
+public class MenuCalculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ MenuCalculadora/src/main/java/com/mycompany/menucalculadora/Principal.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.menucalculadora;
+
+public class Principal {
+    public static void main(String[] args) {
+        MenuCalculadora menu = new MenuCalculadora();
+        menu.executarCalculadora();
+    }
+}
