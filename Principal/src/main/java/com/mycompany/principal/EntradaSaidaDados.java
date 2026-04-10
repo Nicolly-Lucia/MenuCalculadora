86a5b01154cbc607bae6235f3721ceadb20018b8
Nicolly Lucia Da Silva <nicklucia0401@gmail.com>
2026-04-09 21:22:28 -0300

    Add files via upload

---
+++ Principal/src/main/java/com/mycompany/principal/EntradaSaidaDados.java
@@ -0,0 +1,12 @@
+package com.mycompany.principal;
+import javax.swing.JOptionPane;
+
+public class EntradaSaidaDados {
+    public String entradaDados(String mensagemEntrada) {
+        return JOptionPane.showInputDialog(mensagemEntrada);
+    }
+    
+    public void saidaDados(String mensagemSaida) {
+        JOptionPane.showMessageDialog(null, mensagemSaida);
+    }
+}
---
+++ Principal/src/main/java/com/mycompany/principal/Principal.java
@@ -0,0 +1,8 @@
+package com.mycompany.principal;
+
+public class Principal {  
+    public static void main(String[] args) {  
+        MenuCalculadora menu = new MenuCalculadora();  
+        menu.executarCalculadora();  
+    }  
+}
